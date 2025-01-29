3d2569cbb1155843a07cfe86aee601d25855ba75
Vikash-kumar8789 <22amtics293@gmail.com>
2025-01-29 12:26:14 +0530

    solve this errors

--- demo_project/lib/main.dart
+++ demo_project/lib/main.dart
@@ -9,32 +9,28 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyAppHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class MyAppHomePage extends StatefulWidget {
+  const MyAppHomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<StatefulWidget> createState() {
+    return _MyAppHomePageState();
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyAppHomePageState extends State<MyAppHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
     setState(() {
-      _counter++;
+      _counter = _counter + 1;
     });
   }
 
@@ -42,27 +38,30 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Center(
+          child: Text('Flutter Demo Project'),
+        ),
+        backgroundColor: Colors.red,
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          children: [
             const Text(
-              'You have pushed the button this many times:',
+              "You have pushed the button this many times:",
+              style: TextStyle(fontSize: 20),
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              "$_counter",
+              style: const TextStyle(fontSize: 20),
+            ),
+            FloatingActionButton(
+              onPressed: _incrementCounter,
+              tooltip: 'Increment',
+              child: const Icon(Icons.add),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
